068b7c4bfe9f834b36afd623a621a839959457fc
Dinisurumad <dinisurumad@gmail.com>
2024-10-24 23:44:51 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = {
+  assetPrefix: './',
+  basePath: '/Portfolio-website', // Replace 'your-repo-name' with your actual repository name
+};
